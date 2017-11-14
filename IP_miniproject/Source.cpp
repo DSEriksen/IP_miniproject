45630dcf6876fbe6d7628abfc5d10de5cfc4f422
David Eriksen <deriks16@student.aau.dk>
2017-11-14 19:25:38 +0100

    update

--- IP_miniproject/Source.cpp
+++ IP_miniproject/Source.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <opencv2\opencv.hpp>
+#include <cmath>
 
 using namespace cv;
 using namespace std;
 
+# define PI 3.14159265358979323846
 
 bool detectKey(int ms) {
 	int key = waitKey(ms);
@@ -27,6 +29,7 @@ void basicDisPrint(Mat _image) {
 	cout << "pixels: " << _image.rows * _image.cols << endl;
 	cout << "adkaspdk: " << _image.at<Point>(Point(5, 3));
 
+	//opencv scaling
 	/*Mat imgSmall = _image;
 	resize(imgSmall, imgSmall, Size(5, 5));
 	namedWindow("dog small");
@@ -34,6 +37,8 @@ void basicDisPrint(Mat _image) {
 	cout << "Matrix = " << imgSmall << endl;*/
 }
 
+
+//reading an image (thats already been read through imread) and copying each pixel to a new mat
 Mat readImage(Mat _image) {
 
 	cvtColor(_image, _image, COLOR_BGR2GRAY);
@@ -49,19 +54,97 @@ Mat readImage(Mat _image) {
 	return image_forl;
 }
 
+//rotation matrix creation with angle decimals
+Mat createRotationMatrix(double theta, double x, double y, double _scale) {
+	double angle = theta * PI / 180;
+	int cenX = x;
+	int cenY = y;
+	double scale = _scale;
+	double alpha = scale*cos(angle);
+	double beta = scale*sin(angle);
+
+	/*
+	Mat rotMat = Mat(2, 3, CV_64FC1);
+	rotMat.at<double>(0,0) = alpha;
+	rotMat.at<double>(0,1) = beta;
+	rotMat.at<double>(0,2) = ((1 - alpha)*cenX) - (beta*cenY);
+	rotMat.at<double>(1,0) = (beta*(-1));
+	rotMat.at<double>(1,1) = alpha;
+	rotMat.at<double>(1,2) = (beta*cenX) + ((1 - alpha)*cenY);
+	*/
+
+	Mat rotMat = Mat(2, 2, CV_64FC1);
+	rotMat.at<double>(0, 0) = cos(angle);
+	rotMat.at<double>(0, 1) = sin(angle) *(-1);
+	rotMat.at<double>(1, 0) = sin(angle);
+	rotMat.at<double>(1, 1) = cos(angle);
+
+	return rotMat;
+}
+
+Mat rotate(Mat _mat, Mat _rotMat, double x, double y) {
+
+	Mat dst = Mat::zeros(Size(_mat.rows, _mat.cols), _mat.type());
+	_mat.copyTo(dst);
+
+	Mat rotMat = Mat::zeros(Size(_rotMat.rows, _rotMat.cols), _rotMat.type());
+	_rotMat.copyTo(rotMat);
+
+	int p1;
+	int p2;
+	int temp;
+
+	//find point 1
+	//find point 2
+	//tempsave point 1
+	//set point 1 to point 2 
+	//set point 2 to tempsave 
+	
+	for (int y = 0; y < dst.rows; y++) {
+		for (int x = 0; x < dst.cols; x++) {
+			p1 = dst.at<uchar>(x, y);
+			p2 = dst.at<uchar>(
+								(_rotMat.at<double>(0,0)*x) + (_rotMat.at<double>(0,1)*y) - //x * cos(a) + sin(a)
+
+
+			//find point2
+			temp = dst.at<uchar>(
+
+				)
+
+		}
+	}
+
+
+	return dst;
+}
+
 int main(int, char** argv) {
 
 	Mat image = imread("dog.jpg");
 	Mat newImage = readImage(image);
-	Mat rotationMatrix = getRotationMatrix2D(Point(250, 250), 45, 0.5);
+
+	//cheat
+	//Mat rotationMatrix = getRotationMatrix2D(Point(250, 250), 45, 0.7);
+
+	//custom
+	Mat rotMat = createRotationMatrix(45, newImage.rows/2,newImage.cols/2, 1);
+	cout << rotMat;
 
 	namedWindow("dog", CV_WINDOW_AUTOSIZE);
 	imshow("dog", newImage);
 
-	warpAffine(newImage, newImage, rotationMatrix, newImage.size());
+	//cheat
+	//warpAffine(newImage, newImage, rotMat, newImage.size());
+
+	//custom
+	Mat rotatedMat = rotate(newImage, rotMat);
+
+
+	//rotated display
 	namedWindow("dog rotate", CV_WINDOW_AUTOSIZE);
 	imshow("dog rotate", newImage);
-
+	
 
 	waitKey(0);
 	return 0;
---
+++ readme.txt
@@ -0,0 +1,6 @@
+rotation matrix complete
+
+challenge:
+	writing a custom (warpAffine) function is difficult
+	biggest problem is the variable degree that it has to be able to be rotated, as well as the variable point of origin(which would require translation)
+	best method? rotating entire matrix somehow? or finding a point on the image, find a second point with rotation matrix, and then swapping their intensity values?
